dc4dc809722b6ae3d2da66973da991decce223eb
李泽宇 <18666329827lz@gmail.com>
2022-03-13 12:07:00 +0800

    feat: 增加leetcode第21、92、160和876的JavaScript版本解

--- leetcode-solution/leetcode21.js
+++ leetcode-solution/leetcode21.js
@@ -26,3 +26,20 @@ var mergeTwoLists = function (list1, list2) {
     prev.next = list1 === null ? list2 : list1;
     return dummyHead.next;
 };
+
+
+var mergeTwoLists = function (list1, list2) {
+    if (list1 === null) {
+        return list2;
+    }
+    else if (list2 === null) {
+        return list1;
+    }
+    else if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list2, list1.next);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
